a026f9831ace375f62bb1feb0e3edacfe1691f00
Adam DEZIRI <adam.deziri@epitech.eu>
2023-05-13 22:43:55 +0200

    feat: convert the tezos balance to dollars value

--- src/components/dashAsso/Amount.tsx
+++ src/components/dashAsso/Amount.tsx
@@ -1,24 +1,39 @@
 import Button from "@/src/components/Button";
 import { useDappContext } from "@/src/contexts/dapp";
+import convertTezosToUsd from "@/src/utils/convert";
 import getBalanceAssociation from "@/src/utils/getBalanceAssociation";
 import { Text, VStack } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
 
 const AmountWithdraw = () => {
-  const dollarValue = 47.56;
   const { storage } = useDappContext();
-	const value = getBalanceAssociation(storage);
+  const value = getBalanceAssociation(storage);
+  const [dollarValue, setDollarValue] = useState(0.0);
 
-    return ( <VStack>
-              <Text fontSize="50px">{value.toString()} <Text as='u'>XTZ</Text></Text>
-              <Button
-                variant="special"
-                size="xl"
-                id="ipc-landing-navbar-start-button"
-              >
-                Claim
-              </Button>
-              <Text fontSize="15px" mt="9px"> ( {dollarValue} $ )</Text>
-            </VStack>);
-}
+  useEffect(() => {
+    const fetchDollarValue = async () => {
+      const valueInDollars = await convertTezosToUsd(value);
+      if (valueInDollars === undefined)
+        return;
+      setDollarValue(valueInDollars);
+    };
+
+    fetchDollarValue();
+  }, []);
+
+  return (
+    <VStack>
+      <Text fontSize="50px">
+        {value.toString()} <Text as='u'>XTZ</Text>
+      </Text>
+      <Button variant="special" size="xl" id="ipc-landing-navbar-start-button">
+        Claim
+      </Button>
+      <Text fontSize="15px" mt="9px">
+        ( {dollarValue.toFixed(2)} $ )
+      </Text>
+    </VStack>
+  );
+};
 
 export default AmountWithdraw;
--- src/utils/getBalanceAssociation.ts
+++ src/utils/getBalanceAssociation.ts
@@ -2,7 +2,7 @@ import { StorageData } from "@/src/types/types";
 
 const getBalanceAssociation = (storage: StorageData) => {
 	const balance = storage.totalStaked / 1000000;
-	return balance.toString();
+	return balance;
 }
 
 export default getBalanceAssociation;
