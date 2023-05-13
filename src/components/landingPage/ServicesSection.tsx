43bc0dc13c7ff002a0a6fc5d8aefe6c77247e093
Mohammed JBILOU <95973236+Molaryy@users.noreply.github.com>
2023-05-13 15:24:30 +0200

    feat: added 2 buttons for donor & charity

--- src/components/landingPage/ServicesSection.tsx
+++ src/components/landingPage/ServicesSection.tsx
@@ -5,44 +5,7 @@ import colors from "@/src/theme/foundations/colors";
 import { IoWalletOutline } from "react-icons/io5";
 import { useRouter } from "next/router";
 import Button from "../Button";
-
-const ServicesCard = ({
-	title,
-	description,
-	icon,
-	id,
-	position,
-}: {
-	title: string;
-	description: string;
-	icon: string;
-	id: string;
-	position: "left" | "right";
-}) => (
-	<VStack
-		bgGradient={`linear-gradient(135deg, ${position === "left" ? colors.blue[900] : colors.red[900]} 0%, ${
-			position === "left" ? colors.red[900] : colors.blue[900]
-		} 100%)`}
-		p="5px"
-		borderRadius="16px"
-		filter={`drop-shadow(4px 4px 4px ${position === "left" ? "#FF003625" : "#0027FF25"})`}
-		w={{ base: "250px", sm: "425px" }}
-		textAlign="center"
-		id={id}
-	>
-		<VStack bg="blue.50" borderRadius="12px" p={{ base: "32px 24px", sm: "64px 24px" }} spacing="48px">
-			<VStack spacing="24px">
-				<Img src={icon} />
-				<Text variant={`${position === "left" ? "gradient" : "reverseGradient"}`} size="2xl" id={`${id}-title`}>
-					{title}
-				</Text>
-			</VStack>
-			<Text size="xl" id={`${id}-description`}>
-				{description}
-			</Text>
-		</VStack>
-	</VStack>
-);
+import ServicesCard from "../serviceCard";
 
 const ServicesSection = (): JSX.Element => {
 	const isMobile: boolean = useBreakpointValue({ base: true, "2xl": false }) || false;
@@ -94,7 +57,7 @@ const ServicesSection = (): JSX.Element => {
 						buttonType="left-icon"
 						icon={IoWalletOutline}
 						onClick={() => {
-							router.push("/registe/donor");
+							router.push("/register/donor");
 						}}
 					>
 						Start as donor
@@ -114,10 +77,10 @@ const ServicesSection = (): JSX.Element => {
 						buttonType="left-icon"
 						icon={IoWalletOutline}
 						onClick={() => {
-							router.push("/registe/charitie");
+							router.push("/register/charity");
 						}}
 					>
-						Start as charitie
+						Start as charity
 					</Button>
 				</VStack>
 			</Stack>
