9f1c01cda6444ac7c6da43bd495663683c2ea57a
Adam DEZIRI <adam.deziri@epitech.eu>
2023-05-13 09:36:58 +0200

    feat: file structure of the app interface

---
+++ pages/donnor/login.tsx
@@ -0,0 +1,10 @@
+import { VStack } from "@chakra-ui/react";
+
+
+const LoginDonnor = () => (
+	<VStack w="100%" spacing={{ base: "128px", lg: "256px" }} pt="64px" overflowY="hidden">
+      <h1>HEY</h1>
+	</VStack>
+);
+
+export default LoginDonnor;
